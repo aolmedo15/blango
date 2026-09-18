0fd2af60db4975fc4b046babd36d5910566838cb
Alejandro Pérez <aolmedoperez@icloud.com>
2026-09-18 05:08:08 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -64,6 +64,51 @@ class Dev(Configuration):
   #     'django.middleware.clickjacking.XFrameOptionsMiddleware',
   ]
 
+  
+  LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+    },
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "class": "django.utils.log.AdminEmailHandler",
+            "filters": ["require_debug_false"],
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+  }
+
+  ADMINS = [("Ben Shaw", "ben@example.com"), ("Leo Lucio", "leo@example.com")]
+  DJANGO_ADMINS="Ben Shaw,ben@example.com;Leo Lucio,leo@example.com"
+
+  
+
   ROOT_URLCONF = 'blango.urls'
 
   TEMPLATES = [
@@ -114,6 +159,7 @@ class Dev(Configuration):
           'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
       },
   ]
+
 
 
   # Internationalization
