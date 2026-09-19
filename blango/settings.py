efba9aa1a9fc807f5e8fd18aac7bd814bb7bade2
Alejandro Pérez <aolmedoperez@icloud.com>
2026-09-19 06:13:49 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -52,6 +52,7 @@ class Dev(Configuration):
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
+      'debug_toolbar'
   ]
 
   MIDDLEWARE = [
@@ -62,7 +63,11 @@ class Dev(Configuration):
       'django.contrib.auth.middleware.AuthenticationMiddleware',
       'django.contrib.messages.middleware.MessageMiddleware',
   #     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      'debug_toolbar.middleware.DebugToolbarMiddleware',
   ]
+
+  #INTERNAL_IPS = ["192.168.11.179"]
+  INTERNAL_IPS = ["192.168.10.92"]
 
   
   LOGGING = {
