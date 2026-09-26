82c9db03dbb4a61d7e9b5ba6a4ec80fd57f1bde4
Alejandro Pérez <aolmedoperez@icloud.com>
2026-09-26 04:25:17 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -61,6 +61,7 @@ class Dev(Configuration):
       'allauth.socialaccount.providers.google',
       'rest_framework',
       'rest_framework.authtoken',
+      'drf_yasg'
   ]
 
   MIDDLEWARE = [
@@ -136,6 +137,13 @@ class Dev(Configuration):
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ],
+  }
+
+  SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+        "Basic": {"type": "basic"},
+      }
   }
 
   ROOT_URLCONF = 'blango.urls'
